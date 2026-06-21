2427f270b8777074c8e3b3b1cb5dc4e80e75c624
Shuruti Priyama <shuruti1003@gmail.com>
2026-06-21 17:25:18 +0530

    refactor : useTheme hook for consistency

--- frontend/src/context/useTheme.js
+++ frontend/src/context/useTheme.js
@@ -1,7 +1,6 @@
 import { useContext } from 'react'
 import { ThemeContext } from './theme-context'
 
-/** Hook to access { theme, setTheme, toggleTheme } from any component. */
 export function useTheme() {
   const ctx = useContext(ThemeContext)
   if (!ctx) throw new Error('useTheme must be used within a ThemeProvider')
